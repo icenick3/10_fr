b2ec51cabc44311190909ce02232c52b5fc76fa9
kolya <kolyapadda@gmailc.com>
2023-06-06 14:15:58 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from "./conponents/Header/Header";
+import TShirt from "./pages/TShirt/TShirt";
+import Footer from "./conponents/Footer/Footer";
+import './App.css'
+import PurpleHeader from "./conponents/PurpleHeader/PurpleHeader";
+import Zippo from "./pages/Zippo/Zippo";
+import PreFooter from "./conponents/PreFooter/PreFooter";
+import Mbappe from "./pages/Mbappe/Mbappe";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <PurpleHeader/>
+            {/*<Header/>*/}
+            <TShirt/>
+            <Zippo/>
+            <Mbappe/>
+            <PreFooter/>
+            <Footer/>
+        </div>
+    );
+};
 
 export default App;
